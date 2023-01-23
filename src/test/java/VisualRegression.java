cf2ffd83969b2dcfa5a7dacb154d2616eba41ab6
Ali Celik <aliihsancelik1@gmail.com>
2023-01-23 12:05:06 +0000

    latest code

--- src/test/java/VisualRegression.java
+++ src/test/java/VisualRegression.java
@@ -18,14 +18,6 @@ public class VisualRegression {
 
     WebDriver driver;
 
-    @DataProvider(name = "urls")
-    public static Object[][] urls() {
-        return new Object[][]{
-                {"https://demo.guru99.com/test/newtours/index.php", "homePage"},
-                {"https://demo.guru99.com/test/newtours/reservation.php", "flightsPage"}
-        };
-    }
-
     @BeforeTest
     public void setup() {
         try {
@@ -40,18 +32,20 @@ public class VisualRegression {
         driver.get("https://demo.guru99.com/test/newtours/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
         driver.switchTo().frame("gdpr-consent-notice");
         driver.findElement(By.xpath("//span[contains(text(),'Accept All')]")).click();
         driver.switchTo().defaultContent();
     }
 
+
     @Test
-    public void test1() {
+    public void takeEntirePage() {
         ScreenCaptureUtility.takePageScreenShot(driver, "HomePage");
     }
 
     @Test
-    public void test2() {
+    public void takeSpecificElement() {
         WebElement logo = driver.findElement(By.xpath("//img[@alt='Mercury Tours']"));
         ScreenCaptureUtility.takeElementScreenShot(driver, "logo", logo);
     }
@@ -69,6 +63,15 @@ public class VisualRegression {
         Assert.assertTrue(ScreenCaptureUtility.areImagesEqual("expectedHomePage", "actualHomePage"));
     }
 
+
+    @DataProvider(name = "urls")
+    public static Object[][] urls() {
+        return new Object[][]{
+                {"https://demo.guru99.com/test/newtours/index.php", "homePage"},
+                {"https://demo.guru99.com/test/newtours/reservation.php", "flightsPage"}
+        };
+    }
+
     @Test(dataProvider = "urls")
     public void prepareBaseLine(String url, String name) {
         driver.get(url);
@@ -81,6 +84,7 @@ public class VisualRegression {
         ScreenCaptureUtility.takePageScreenShot(driver, name);
         Assert.assertTrue(ScreenCaptureUtility.areImagesEqual(name, name));
     }
+
 
     @AfterTest
     public void tearDown() {
